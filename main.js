512bbd6cbea898947b449b5bb3f416534d43bb22
Jamshiddin <jbm050690@gmail.com>
2025-12-07 23:43:24 +0500

    my projectga bazi stylelar berdim

---
+++ main.js
@@ -0,0 +1,32 @@
+const darkEl = document.getElementById("dark");
+const modeIcon = document.getElementById("modeIcon");
+// Dark mode menu
+const MODE = "mode";
+let isDark = false;
+
+darkEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  isDark = !isDark;
+  localStorage.setItem(MODE, isDark);
+  changeMode();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentMode = localStorage.getItem(MODE);
+  if (currentMode !== null) {
+    isDark = currentMode === "true";
+  }
+  changeMode();
+});
+
+const changeMode = () => {
+  if (isDark) {
+    document.body.classList.add("dark_mode");
+    document.body.classList.remove("light_mode");
+    modeIcon.src = "/images/Sun_mode.svg";
+  } else {
+    document.body.classList.add("light_mode");
+    document.body.classList.remove("dark_mode");
+    modeIcon.src = "/images/Dark_mode.svg";
+  }
+};
